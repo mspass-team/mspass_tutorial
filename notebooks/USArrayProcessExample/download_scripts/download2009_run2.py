920d047e8c6b5ee46ac7350ab37d49a6c4afe083
Gary Pavlis <pavlis@indiana.edu>
2025-04-05 05:26:02 -0400

    Add download scripts for reference

---
+++ notebooks/USArrayProcessExample/download_scripts/download2009_run2.py
@@ -0,0 +1,19 @@
+import time
+from rfteledownload import read_centers
+from rfteledownload import get_catalog
+from rfteledownload import download_events
+
+centers=read_centers()
+yr=2009
+t0=time.time()
+print("Running get_catalog")
+cat=get_catalog(centers,year=yr)
+fname=("events_%d.xml" % yr)
+cat.write(fname,format='QUAKEML')
+t1=time.time()
+print("Elapsed time for to fetch catalog=",t1-t0)
+print("Running download_events")
+t1=time.time()
+download_events(cat,yeartag=str(yr),firstid=544)
+t2=time.time()
+print("Elapsed time to download events=",t2-t1)
